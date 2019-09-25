f8586f760ed1b6cd2e349d9c4939597afa48be2c
Abdullah <irsalabd@gmail.com>
2019-09-25 10:02:34 +0600

    [app] update app config setup

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from telegram import telegram
 from . import config, management
+from urllib.parse import urlparse
 
 logger = logging.getLogger(__name__)
 logger.setLevel(config.LOGLEVEL)
@@ -15,14 +16,21 @@ db = SQLAlchemy()
 
 def make_app():
     bot = telegram.Bot.by(token=config.BOT_API_TOKEN)
+    
+    target_webhook_url = config.BOT_WEBHOOK_URL
+    if not urlparse(target_webhook_url).path:
+        target_webhook_url += '/{}/'.format(bot._api_token)
+
     webhook_url = bot.webhookinfo().url
     logger.debug('WEBHOOK_URL: %r', webhook_url)
-    if webhook_url != config.BOT_WEBHOOK_URL:
+
+    if webhook_url != target_webhook_url:
         bot.delete_webhook()
-        r = bot.set_webhook(config.BOT_WEBHOOK_URL)
+        r = bot.set_webhook(target_webhook_url)
         logger.info('SET_WEBHOOK: %r', r)
 
     app = Flask(__name__)
+    app.config.from_object(config)
     app.cli.add_command(management.bootstrap)
     app.config['SQLALCHEMY_DATABASE_URI'] = config.DATABASE_URL
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
