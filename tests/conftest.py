695cb12a317f296b1b6e63584546569ac61a1eb4
Abdullah <irsalabd@gmail.com>
2019-09-25 10:14:24 +0600

    [tests] move fixtures to conftest module

---
+++ tests/conftest.py
@@ -0,0 +1,25 @@
+
+
+import os
+import tempfile
+import pytest 
+
+
+@pytest.fixture(scope='module')
+def app():
+    from app import make_app
+    app = make_app()
+    with app.app_context() as ctx:
+        yield app
+
+
+@pytest.fixture
+def client(app):
+    db_fd, app.config['DATABASE'] = tempfile.mkstemp()
+    app.config['TESTING'] = True
+
+    with app.test_client() as client:
+        yield client
+
+    os.close(db_fd)
+    os.unlink(app.config['DATABASE'])
